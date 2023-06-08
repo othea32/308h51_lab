1eeef4d602a2acbccdf0cf862fa1c522cb50b0bf
othea32 <othea32@gmail.com>
2023-06-08 08:27:20 -0700

    lab answers 6 -10

--- app.js
+++ app.js
@@ -65,4 +65,56 @@ const numArgs = function() {
 
 console.log(numArgs(9, 10, 11, 12, 13));
 
+// 7.
+function reverseString(string) {
+  let reversedString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  return reversedString;
+}
+
+console.log(reverseString("rockstar"));
+
+
+
+
+// 8.
+const longestStringInArray = function(array) {
+  let longestString = "";
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length > longestString.length) {
+      longestString = array[i];
+    }
+  }
+  return longestString.length;
+  }
+
+console.log(longestStringInArray(["hello", "goodbye", "good day"]));
+
+// 9.
+function stringsLongerThan(array, number) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length > number) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+
+console.log(stringsLongerThan(["hello", "goodbye", "good day"], 7));
+
+// 10.
+// Define a function, as a function expression, addList that accepts any quantity of numbers as arguments, adds them together, and returns the resulting sum. Assume all parameters will be numbers. If called with no arguments, return 0 (zero).
+
+const addList = function() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+
+console.log(addList(1, 2, 3, 4, 5));
 
